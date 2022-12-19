f8851b2ac076cc3c818f9b77c2a402a08f79321b
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2022-12-19 16:55:27 +0530

    added minutes and hours

--- script.js
+++ script.js
@@ -3,23 +3,50 @@ const stopBttn = document.getElementById('stop-bttn')
 const resetBttn = document.getElementById('reset-bttn')
 let appendSec = document.getElementById('sec')
 let appendTen = document.getElementById('ten')
+let appendMins = document.getElementById('mins')
+let appendHours = document.getElementById('hours')
 var interval
-var tens = "00"
-var sec = "00"
+var tens = 00
+var sec = 00
+var mins = 00
+var hours = 00
 
-
-startBttn.addEventListener(click, function startCount() {
+startBttn.addEventListener("click", function startCount() {
     interval = setInterval(startCountDown, 10)
 })
 function startCountDown() {
     tens++
     if(tens <= 9){
-        appendTen.innerText += "0" + tens
+        appendTen.innerText = "0" + tens
+        appendSec.innerText =  sec 
     }
     else if (tens > 99){
-        tens = 0
-        appendTen.innerText  += "0" + tens
-        appendSec.innerText  += "0" + sec
+        tens = "0"
+        sec++
+        appendTen.innerText  = "0" + tens
+        appendSec.innerText = "0" +  sec
+        if (sec > 9){
+            appendSec.innerText = sec
+        }else if (sec >= 60){
+            appendSec.innerText = "00"
+            mins++
+            appendMins.innerText = "0" + mins
+        }
 
+    }else {
+        appendSec.innerText = sec 
+        appendTen.innerText = tens 
     }
 }
+resetBttn.addEventListener("click",function() {
+    interval = clearInterval
+    sec = "00"
+    tens = "00"
+    appendSec.innerText = sec
+    appendTen.innerText = tens
+})
+stopBttn.addEventListener("click", function() {
+    interval = clearInterval
+    appendSec = sec
+    appendTen = tens
+})
